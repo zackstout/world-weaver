caa42a87b8fcd3ecbc0f5484cade0b0aab917e59
Zack Stout <zackstout@gmail.com>
2017-11-18 15:24:39 -0600

    ok we have our cannon shooting balls, good

--- SOLO/WORLD WEAVER/server/public/scripts/controllers/play.controller.js
+++ SOLO/WORLD WEAVER/server/public/scripts/controllers/play.controller.js
@@ -6,40 +6,31 @@ myApp.controller('PlayController', function(UserService, WorldService, $http) {
   var vm = this;
   vm.world = [];
 
-  //i am seriously confused: it shows vm.ws as having a "world" property, but won't let us grab it:
-  // function getWorld() {
-  //   WorldService.getWorld();
-  //   vm.ws = WorldService;
-  //   console.log(vm.ws.world);
-  // }
-
   function getWorld() {
     $http.get('/worlds').then(function (response) {
       console.log('got world!');
       vm.world = response.data;
-      var w = vm.world[0];
-      x = w.obstacle_x;
-      y = w.obstacle_y;
-      h = w.obstacle_h;
-      //lol i was reusing 'w':
-      wid = w.obstacle_w;
       console.log(vm.world);
-      // console.log(x,y,w,h);
     }).catch(function (err) {
       console.log('whooooops');
     });
   }
 
   getWorld();
-  setTimeout(doMatter, 2000);
-
+  setTimeout(doMatter, 300);
 
   function doMatter() {
     var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
+    Mouse = Matter.Mouse,
+    MouseConstraint = Matter.MouseConstraint,
+    Body = Matter.Body,
+    Events = Matter.Events,
     Bodies = Matter.Bodies;
     var engine = Engine.create();
+    var world = engine.world;
+    var obstacle, cannon, cannonball, bucket;
 
     // create a renderer
     var render = Render.create({
@@ -47,20 +38,83 @@ myApp.controller('PlayController', function(UserService, WorldService, $http) {
       engine: engine
     });
 
-    // var boxA = Bodies.rectangle(400, 200, 80, 80);
-    // var boxB = Bodies.rectangle(450, 50, 80, 80);
-    // console.log(x,y,h,wid);
+
+    var mouse = Mouse.create(render.canvas);
+    var mc = MouseConstraint.create(engine, {
+      mouse: mouse
+    });
+    console.log(mc);
+    Events.on(mc, "mousedown", function() {
+      console.log('hello sir', mouse.position.x, mouse.position.y);
+      cannonball = Bodies.circle(cannon.position.x + 40*Math.cos(cannon.angle), cannon.position.y + 40*Math.sin(cannon.angle), 15, {restitution: 1, friction: 0});
+      World.add(world, cannonball);
+      Body.applyForce(cannonball, {x: cannon.position.x, y: cannon.position.y}, {x: 0.04*Math.cos(cannon.angle), y: 0.04*Math.sin(cannon.angle)});
+
+    });
+
+
+    // console.log(vm.world);
+    var ourWorld = vm.world[0];
+    cannon = Bodies.rectangle(ourWorld.start_x, ourWorld.start_y, 40, 20, {isStatic: true});
+    bucket = Bodies.rectangle(ourWorld.end_x, ourWorld.end_y, 30, 30, {isStatic: true});
+    // console.log(cannon, bucket);
+    World.add(world, [cannon, bucket]);
 
     for (var i=0; i<vm.world.length; i++) {
       var wor = vm.world[i];
-      var obstacle = Bodies.rectangle(wor.x, wor.y, wor.w, wor.h, { isStatic: true });
-      World.add(engine.world, [obstacle]);
+      if (wor.type == "rect") {
+        obstacle = Bodies.rectangle(wor.x, wor.y, wor.w, wor.h, { isStatic: true });
+      } else if (wor.type == "ellipse") {
+        obstacle = Bodies.circle(wor.x, wor.y, wor.h, { isStatic: true });
+      }
+      World.add(world, [obstacle]);
     }
-
 
     Engine.run(engine);
     Render.run(render);
   }
+
+  // function setup() {
+  // console.log('hi', vm.world);
+  // var Engine = Matter.Engine,
+  // Render = Matter.Render,
+  // World = Matter.World,
+  // Bodies = Matter.Bodies;
+  // var engine = Engine.create();
+  // var world = engine.world;
+  // var obstacle;
+  // var cannon;
+  // var bucket;
+  // var canvas = createCanvas(1000,1000);
+  //
+  // Engine.run(engine);
+  //
+  // var ourWorld = vm.world[0];
+  // cannon = Bodies.rectangle(ourWorld.start_x, ourWorld.start_y, 40, 20, {isStatic: true});
+  // bucket = Bodies.rectangle(ourWorld.end_x, ourWorld.end_y, 30, 30, {isStatic: true});
+  // // console.log(cannon, bucket);
+  // World.add(world, [cannon, bucket]);
+  //
+  // for (var i=0; i<vm.world.length; i++) {
+  //   var wor = vm.world[i];
+  //   if (wor.type == "rect") {
+  //     obstacle = Bodies.rectangle(wor.x, wor.y, wor.w, wor.h, { isStatic: true });
+  //   } else if (wor.type == "ellipse") {
+  //     obstacle = Bodies.circle(wor.x, wor.y, wor.h, { isStatic: true });
+  //   }
+  //   World.add(world, [obstacle]);
+  // }
+  //
+  // }
+  //
+  // function draw() {
+  //   background(160);
+  //   rect(100, 100, 200, 200);
+  //   // World.add(world, obstacle);
+  // // rect(box1.position.x,box1.position.y, 20, 20);
+  // // rect(obstacle.position.x, obstacle.position.y, 100, 100);
+  //
+  // }
 
 });
 
@@ -78,7 +132,7 @@ myApp.controller('PlayController', function(UserService, WorldService, $http) {
 // var box1 = Bodies.rectangle(200, 100, 20, 20);
 // var obstacle = Bodies.rectangle(x, y, h, w, {isStatic: true});
 //
-//
+// //
 // function setup() {
 // console.log('hi');
 // var canvas = createCanvas(1000,1000);
