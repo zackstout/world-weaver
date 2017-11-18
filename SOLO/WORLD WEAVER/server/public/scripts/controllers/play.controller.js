d3d2579be53632e1a0ba8d5b17fcac29b7238cbe
Zack Stout <zackstout@gmail.com>
2017-11-18 17:18:18 -0600

    some basic editing functionality is up and running, i.e. adding blocks to the world and then seeing them appear in your test screen

--- SOLO/WORLD WEAVER/server/public/scripts/controllers/play.controller.js
+++ SOLO/WORLD WEAVER/server/public/scripts/controllers/play.controller.js
@@ -1,23 +1,146 @@
 
-var x, y, h, wid;
-
 myApp.controller('PlayController', function(UserService, WorldService, $http) {
   console.log('playController created');
   var vm = this;
   vm.world = [];
+  vm.newWorld = {
+    start_x: 0,
+    start_y: 0,
+    end_x: 0,
+    end_y: 0,
+    obstacles: []
+  };
+  vm.newObstacle = {
+    x: 0,
+    y: 0,
+    h: 0,
+    w: 0,
+    type: 'rect'
+  };
 
-  function getWorld() {
+  vm.showWorld = function() {
+    console.log('clickin new world');
+    doMatterStart();
+  };
+
+  vm.addWorld = function(world) {
+    $http.post('/worlds', world).then(function (response) {
+      console.log('making world!', world);
+    }).catch(function (err) {
+      console.log('whooooops');
+    });
+  };
+
+  vm.addObstacle = function(obstacle) {
+    console.log(obstacle);
+    vm.newWorld.obstacles.push(obstacle);
+    //reset object to avoid over-data-binding:
+    doMatterStart();
+    vm.newObstacle = {
+      x: 0,
+      y: 0,
+      h: 0,
+      w: 0,
+      type: 'rect'
+    };
+  };
+
+  //get data necessary to construct the world from the DB:
+  vm.getWorld = function() {
     $http.get('/worlds').then(function (response) {
       console.log('got world!');
       vm.world = response.data;
       console.log(vm.world);
+      //call doMatter here; shouldn't need setTimeout if you do it like this:
+      doMatter();
     }).catch(function (err) {
       console.log('whooooops');
     });
-  }
+  };
 
-  getWorld();
-  setTimeout(doMatter, 300);
+  // getWorld();
+  //ensure we get our data before trying to populate the world:
+  // setTimeout(doMatter, 200);
+
+  function doMatterStart() {
+    console.log(vm.newWorld.obstacles);
+    var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Mouse = Matter.Mouse,
+    MouseConstraint = Matter.MouseConstraint,
+    Body = Matter.Body,
+    Events = Matter.Events,
+    Bodies = Matter.Bodies;
+    var engine = Engine.create();
+    var world = engine.world;
+    var obstacle, cannon, cannonball, bucket, x=0;
+
+    // create a renderer
+    var render = Render.create({
+      element: document.body,
+      engine: engine
+    });
+
+    //rotate the cannon:
+    window.onkeydown = function(e) {
+      x-=0.14;
+      Body.setAngle(cannon, x);
+    };
+
+    //listen for mouse-clicks, fire cannon:
+    var mouse = Mouse.create(render.canvas);
+    var mc = MouseConstraint.create(engine, {
+      mouse: mouse
+    });
+    console.log(mc);
+    Events.on(mc, "mousedown", function() {
+      console.log('hello sir', mouse.position.x, mouse.position.y);
+      cannonball = Bodies.circle(cannon.position.x + 40*Math.cos(cannon.angle), cannon.position.y + 40*Math.sin(cannon.angle), 15, {restitution: 1, friction: 0});
+      World.add(world, cannonball);
+      Body.applyForce(cannonball, {x: cannon.position.x, y: cannon.position.y}, {x: 0.04*Math.cos(cannon.angle), y: 0.04*Math.sin(cannon.angle)});
+    });
+
+    //listen for collisions:
+    Events.on(engine, 'collisionStart', function(event) {
+      var pairs = event.pairs;
+      for (var i = 0, j = pairs.length; i != j; i++) {
+        var pair = pairs[i];
+        if (pair.bodyA === bucket) {
+          console.log('collision dog <3');
+          world.gravity.y = -world.gravity.y;
+        }
+        else if (pair.bodyB === bucket) {
+          console.log('whatup');
+          world.gravity.y = -world.gravity.y;
+        }
+      }
+    });
+
+    //add cannon and bucket:
+    // console.log(vm.world);
+    var ourWorld = vm.newWorld;
+    cannon = Bodies.rectangle(ourWorld.start_x, ourWorld.start_y, 40, 20, {isStatic: true});
+    bucket = Bodies.rectangle(ourWorld.end_x, ourWorld.end_y, 30, 30, {isStatic: true});
+    // console.log(cannon, bucket);
+    World.add(world, [cannon, bucket]);
+
+    //add obstacles:
+    for (var i=0; i<vm.newWorld.obstacles.length; i++) {
+      var wor = vm.newWorld.obstacles[i];
+      if (wor.type == "rect") {
+        obstacle = Bodies.rectangle(wor.x, wor.y, wor.w, wor.h, { isStatic: true });
+      } else if (wor.type == "ellipse") {
+        obstacle = Bodies.circle(wor.x, wor.y, wor.h, { isStatic: true });
+      }
+      World.add(world, [obstacle]);
+    }
+
+    Engine.run(engine);
+    Render.run(render);
+  } //end doMatterStart
+
+
 
   function doMatter() {
     var Engine = Matter.Engine,
@@ -30,7 +153,7 @@ myApp.controller('PlayController', function(UserService, WorldService, $http) {
     Bodies = Matter.Bodies;
     var engine = Engine.create();
     var world = engine.world;
-    var obstacle, cannon, cannonball, bucket;
+    var obstacle, cannon, cannonball, bucket, x=0;
 
     // create a renderer
     var render = Render.create({
@@ -38,7 +161,13 @@ myApp.controller('PlayController', function(UserService, WorldService, $http) {
       engine: engine
     });
 
+    //rotate the cannon:
+    window.onkeydown = function(e) {
+      x-=0.14;
+      Body.setAngle(cannon, x);
+    };
 
+    //listen for mouse-clicks, fire cannon:
     var mouse = Mouse.create(render.canvas);
     var mc = MouseConstraint.create(engine, {
       mouse: mouse
@@ -49,10 +178,25 @@ myApp.controller('PlayController', function(UserService, WorldService, $http) {
       cannonball = Bodies.circle(cannon.position.x + 40*Math.cos(cannon.angle), cannon.position.y + 40*Math.sin(cannon.angle), 15, {restitution: 1, friction: 0});
       World.add(world, cannonball);
       Body.applyForce(cannonball, {x: cannon.position.x, y: cannon.position.y}, {x: 0.04*Math.cos(cannon.angle), y: 0.04*Math.sin(cannon.angle)});
-
     });
 
+    //listen for collisions:
+    Events.on(engine, 'collisionStart', function(event) {
+      var pairs = event.pairs;
+      for (var i = 0, j = pairs.length; i != j; i++) {
+        var pair = pairs[i];
+        if (pair.bodyA === bucket) {
+          console.log('collision dog <3');
+          world.gravity.y = -world.gravity.y;
+        }
+        else if (pair.bodyB === bucket) {
+          console.log('whatup');
+          world.gravity.y = -world.gravity.y;
+        }
+      }
+    });
 
+    //add cannon and bucket:
     // console.log(vm.world);
     var ourWorld = vm.world[0];
     cannon = Bodies.rectangle(ourWorld.start_x, ourWorld.start_y, 40, 20, {isStatic: true});
@@ -60,6 +204,7 @@ myApp.controller('PlayController', function(UserService, WorldService, $http) {
     // console.log(cannon, bucket);
     World.add(world, [cannon, bucket]);
 
+    //add obstacles:
     for (var i=0; i<vm.world.length; i++) {
       var wor = vm.world[i];
       if (wor.type == "rect") {
@@ -70,9 +215,33 @@ myApp.controller('PlayController', function(UserService, WorldService, $http) {
       World.add(world, [obstacle]);
     }
 
+    //for a spinner:
+    // var z = 0;
+    // function rotateG2() {
+    // z += 0.04;
+    // Body.setAngle(bucket, z);
+    //
+    // }
+    // setInterval(rotateG2, 100);
+
     Engine.run(engine);
     Render.run(render);
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   // function setup() {
   // console.log('hi', vm.world);
--- SOLO/WORLD WEAVER/server/routes/world.router.js
+++ SOLO/WORLD WEAVER/server/routes/world.router.js
@@ -31,5 +31,29 @@ router.get('/', function(req, res) {
   });
 }); //END GET ROUTE
 
+router.post('/', function(req, res) {
+  console.log(req.body);
+  var newWorld = req.body;
+  pool.connect(function(err, db, done) {
+    if(err) {
+      console.log('Error connecting', err);
+      res.sendStatus(500);
+    } else {
+      //we connected to DB
+      // var queryText = 'SELECT * FROM "worlds" WHERE "id"=1;';
+      var queryText = 'INSERT INTO "worlds" ("start_x", "start_y", "end_x", "end_y") VALUES ($1, $2, $3, $4);';
+      db.query(queryText, [newWorld.start_x, newWorld.start_y, newWorld.end_x, newWorld.end_y], function(err, result){
+        done();
+        if(err) {
+          console.log('Error making query', err);
+          res.sendStatus(500);
+        } else {j
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+}); //END POST ROUTE
+
 
 module.exports = router;
