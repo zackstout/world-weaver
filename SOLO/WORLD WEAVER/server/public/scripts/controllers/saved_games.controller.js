70edd2b9e77a268b231d49d8f51f2dbf37ffeb6c
Zack Stout <zackstout@gmail.com>
2017-11-29 22:52:19 -0600

    ahhh yes once again i had forgotten layout-wrap

--- SOLO/WORLD WEAVER/server/public/scripts/controllers/saved_games.controller.js
+++ SOLO/WORLD WEAVER/server/public/scripts/controllers/saved_games.controller.js
@@ -9,6 +9,13 @@ myApp.controller('SavedController', function(UserService, $http, $location, Worl
   vm.obstaclesSaved = [];
   vm.savedWorldIds = [];
 
+
+    vm.faves = [];
+
+    vm.obstacles = [];
+
+    vm.faveIds = [];
+
   vm.editWorld = function(world) {
     console.log(world);
     EditService.newWorld = false;
@@ -48,10 +55,101 @@ myApp.controller('SavedController', function(UserService, $http, $location, Worl
             obstacles: []
           });
         }
+        setTimeout(logCanvasSaved, 200);
         vm.getSavedObstacles();
       });
     };
 
     vm.getSavedWorlds();
+
+
+    function logCanvasSaved() {
+      var can = document.getElementsByTagName("canvas");
+      console.log(can);
+      for (var i=0; i<can.length;i++) {
+        var canvas = can[i];
+        // console.log(canvas.id.slice(6));
+        var canvasId = canvas.id.slice(5);
+
+        var ctx = canvas.getContext("2d");
+
+        ctx.fillStyle = 'lightblue';
+        ctx.fillRect(0,0,270,200);
+
+
+        for (var j=0; j<vm.savedWorldIds.length; j++) {
+          if (vm.savedWorldIds[j].id == canvasId) {
+
+            // console.log(vm.allWorldIds[j]);
+            var cannonX = vm.savedWorldIds[j].start_x/3;
+            var cannonY = vm.savedWorldIds[j].start_y/3;
+            var bucketX = vm.savedWorldIds[j].end_x/3;
+            var bucketY = vm.savedWorldIds[j].end_y/3;
+
+            if (vm.savedWorldIds[j].portals) {
+              var port1 = vm.savedWorldIds[j].portals.y1/3;
+              var port2 = vm.savedWorldIds[j].portals.y2/3;
+              ctx.fillStyle = 'purple';
+              ctx.beginPath();
+              ctx.arc(265, port1, 5, 0, 2*Math.PI);
+              ctx.stroke();
+              ctx.fill();
+
+              ctx.beginPath();
+              ctx.arc(5, port2, 5, 0, 2*Math.PI);
+              ctx.stroke();
+              ctx.fill();
+            }
+
+            for (var k=0; k<vm.savedWorldIds[j].obstacles.length; k++) {
+              var obs = vm.savedWorldIds[j].obstacles[k];
+              var x = obs.x/3, y = obs.y/3, h = obs.h/3, w = obs.w/3, a = obs.a;
+              ctx.fillStyle = 'blue';
+
+              if (obs.type == 'rect') {
+                ctx.translate(x, y);
+                ctx.rotate(a*Math.PI/180);
+                ctx.fillRect(-w/2, -h/2, w, h);
+                ctx.rotate(-a*Math.PI/180);
+                ctx.translate(-x, -y);
+              } else if (obs.type == 'circle') {
+                ctx.beginPath();
+                ctx.arc(x, y, h, 0, 2*Math.PI);
+                ctx.stroke();
+                ctx.fill();
+              }
+
+            }
+
+            //draw cannon and bucket:
+            ctx.fillStyle = 'yellow';
+            //it's x, y, width, height:
+            ctx.fillRect(cannonX-8, cannonY-4, 16, 8);
+            ctx.fillStyle = 'green';
+            ctx.fillRect(bucketX-5, bucketY-5, 10, 10);
+
+            // ctx.moveTo()
+          }
+        }
+
+        //draw border:
+        ctx.moveTo(0,0);
+        ctx.lineTo(0,200);
+        ctx.stroke();
+
+        ctx.moveTo(0,0);
+        ctx.lineTo(270,0);
+        ctx.stroke();
+
+        ctx.moveTo(270,0);
+        ctx.lineTo(270,200);
+        ctx.stroke();
+
+        ctx.moveTo(270,200);
+        ctx.lineTo(0,200);
+        ctx.stroke();
+
+      }
+    }
 
 });
